ac8b569b9f8e28f53d141490dde72d30433018a3
Trần Tuấn Vũ <102710535+TuanVuDS@users.noreply.github.com>
2024-09-26 23:37:47 +0700

    Update test_main.py

--- starter/test_main.py
+++ starter/test_main.py
@@ -52,3 +52,24 @@ def test_post_sample_greater_than_50K():
     r = client.post("/", data=json.dumps(data))
     assert r.status_code == 200
     assert r.json() == {'predict': '> 50K'}
+    
+def test_post_sample_less_and_equal_than_50K_part_time():
+    data = {
+        "age": 30,
+        "workclass": "Private",
+        "fnlgt": 200000,
+        "education": "Some-college",
+        "education_num": 10,
+        "marital_status": "Never-married",
+        "occupation": "Sales",
+        "relationship": "Not-in-family",
+        "race": "White",
+        "sex": "Male",
+        "capital_gain": 0,
+        "capital_loss": 0,
+        "hours_per_week": 25,  # Số giờ làm việc ít
+        "native_country": "United-States"
+    }
+    r = client.post("/", data=json.dumps(data))
+    assert r.status_code == 200
+    assert r.json() == {'predict': '<= 50K'}
